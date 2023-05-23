d896473e0447f741570409255e9a726a7b77bb11
arscholtes <alex.scholtes@icloud.com>
2023-05-23 06:56:58 -0500

    media added and readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Ottergram</h1>
       </header>
+      <ul>
+        <li>
+          <button>
+            <img src={Barry} alt='Barry'  />
+              <p>Barry</p>
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
